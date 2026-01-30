d4e8a7b9be328fdc4bec25099413a04b8c7e2be5
vickybanti <olamuyiwavictor55@gmail.com>
2026-01-31 00:15:24 +0100

    added arcject toastify message

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,8 +1,28 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 import {BACKEND_BASE_URL} from '@/constants'
 import {ListResponse} from "@/types";
+import {HttpError} from "@refinedev/core";
+
+if(!BACKEND_BASE_URL) throw new Error('BACKEND_BASE_URL is not defined')
+
+const buildHttpError = async ( response: Response) :Promise<HttpError> => {
+    let message = "Request Failed"
+    
+    try {
+        const payload = (await response.json()) as {message? : string}
+
+        if(payload?.message) message = payload.message;
+    } catch (e) {
+
+    }
+    return {
+        message,
+        statusCode: response.status,
+    }
+}
 
 const options: CreateDataProviderOptions = {
+
     getList:{
         getEndpoint:({ resource }) => resource,
 
@@ -10,7 +30,7 @@ const options: CreateDataProviderOptions = {
             const page = pagination?.currentPage ?? 1;
             const pageSize = pagination ?.pageSize ?? 10;
 
-            const params: Record<string, string|number> = {page, lmit:pageSize};
+            const params: Record<string, string|number> = {page, limit:pageSize};
             filters?.forEach((filter) => {
                 const field = 'field' in filter ? filter.field:'';
 
@@ -24,12 +44,16 @@ const options: CreateDataProviderOptions = {
             return params;
         },
         mapResponse: async (response) => {
-            const payload:ListResponse = await response.json();
+            if(!response.ok) throw await buildHttpError(response);
+
+            const payload:ListResponse = await response.clone().json();
 
             return payload.data ?? [];
         },
         getTotalCount : async(response) => {
-            const payload: ListResponse = await response.json();
+            if(!response.ok) throw await buildHttpError(response);
+
+            const payload: ListResponse = await response.clone().json();
 
             return payload.pagination?.total ?? payload.data?.length ?? 0;
         }
